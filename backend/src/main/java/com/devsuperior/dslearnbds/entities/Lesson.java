77fc56046e1fff35188276a05cfc510528eb2c2a
Lucas Figueiredo Bof <lucasfbof@gmail.com>
2021-01-10 17:51:44 -0300

    Reply

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Lesson.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Lesson.java
@@ -43,6 +43,9 @@ public abstract class Lesson implements Serializable {
 	
 	@OneToMany(mappedBy = "lesson")
 	private List<Deliver> deliveries = new ArrayList<>();
+	
+	@OneToMany(mappedBy = "lesson")
+	private List<Topic> topics = new ArrayList<>(); 
 
 	public Lesson() {
 	}
@@ -92,6 +95,10 @@ public abstract class Lesson implements Serializable {
 	
 	public List<Deliver> getDeliveries() {
 		return deliveries;
+	}
+	
+	public List<Topic> getTopics() {
+		return topics;
 	}
 
 	@Override
